e113c8b7ca4bd39f637a07c2bf2050a611def211
sirvi09 <solankilalit538@gmail.com>
2026-06-21 17:28:08 +0530

    logo issue solved

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import logo from '../assets/logo.png'
+import logo from '../assets/Logo.png'
 import Search from './Search'
 import { Link, useLocation,useNavigate } from 'react-router-dom'
 import { FaRegCircleUser } from "react-icons/fa6";
